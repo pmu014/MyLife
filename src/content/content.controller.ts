fc66a7c922c7dd142de53f6d28d57a2fbd8c12f8
Minuk <ansems@naver.com>
2024-02-14 11:16:58 +0900

    feat content sub

--- src/content/content.controller.ts
+++ src/content/content.controller.ts
@@ -1,4 +1,13 @@
 import { Controller } from '@nestjs/common';
+import { ContentService } from './content.service';
 
 @Controller('content')
-export class ContentController {}
+export class ContentController {
+    constructor(private contentService: ContentService) {}
+
+    @Get('/movie')
+    @Get('/drama')
+    @Get('/animation')
+    @Get('/others')
+
+}
