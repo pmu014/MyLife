3cc08b56c1f8ced5752c00df9c214d26e07c61bb
Minuk <ansems@naver.com>
2024-02-17 10:19:07 +0900

    feat content.service

--- src/content/content.service.ts
+++ src/content/content.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { CreateContentDto } from './dto/create-content.dto';
+import { Content } from './content.entity';
 
 @Injectable()
-export class ContentService {}
+export class ContentService {
+constructor(
+    private contentRepository: ContentRepository,
+) {}
+
+    createContent(CreateContentDto: CreateContentDto): Promise<Content>{
+        return this.contentRepository.createContent(CreateContentDto)
+    }
+}
