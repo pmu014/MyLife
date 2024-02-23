a6e595b221d94c2ce0a789c372482f1b0c80961b
Minuk <ansems@naver.com>
2024-02-23 13:42:48 +0900

    feat getContent

--- src/content/content.controller.ts
+++ src/content/content.controller.ts
@@ -1,7 +1,10 @@
-import { Body, Controller, Get, Post, Render, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Render, UsePipes, ValidationPipe } from '@nestjs/common';
 import { ContentService } from './content.service';
 import { CreateContentDto } from './dto/create-content.dto';
 import { Content } from './content.entity';
+import { CreateContentValidationPipe } from './pipes/content-status-validation.pipe';
+import { ContentDivision } from './content-division.enum';
+
 @Controller('content')
 export class ContentController {
     constructor(private contentService: ContentService) {}
@@ -9,6 +12,11 @@ export class ContentController {
     @Get()
     @Render('content')
     root(){
+        return {message: "hello"}
+    }
+    //파이프내용 수정
+    @Get('/:div')
+    getContent(@Param('div', CreateContentValidationPipe) div: ContentDivision ){
 
     }
     //contentDto+pipes , entity 날짜수정, 
