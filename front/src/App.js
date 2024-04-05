f9f48f35ec18c9f1bb073b3443d79e7e86e6ca9b
Minuk <ansems@naver.com>
2024-04-05 16:08:41 +0900

    front feat: header

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
+import styled from 'styled-components'
 import './App.css';
+
+
+const HeaderSection = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 3fr 1fr;
+  width: 100%;
+  /* justify-content: center; */
+  
+`
+const HeaderContainer = styled.div`
+  /* background-color: red; */
+`
+const Title = styled.p`
+  font-size: 60px;
+  margin: 30px;
+  text-align: center;
+  align-content: center;
+  justify-content: center;
+`
+
+const HeaderBox = styled.div`
+  display: flex;
+  align-content: center;
+  justify-content: center;
+`;
+
+const TitleBox = styled.div`
+  width: 200px;
+  height: 100px;
+  border: 1px solid;
+  border-radius: 25% 10%;;
+  margin: 20px;
+  /* align-items: center; */
+  justify-content: center;
+  align-content: center;
+  text-align: center;
+
+  &:hover {
+    color: crimson;
+  }
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //타이틀별 페이지 이동 필요
+    <HeaderSection>
+      <empty></empty>
+      <HeaderContainer>
+        <Title>Minuk's Life</Title>
+        <HeaderBox>
+          <TitleBox>Content</TitleBox>
+          <TitleBox>Sports</TitleBox>
+          <TitleBox>Health</TitleBox>
+          <TitleBox>Diary</TitleBox>
+        </HeaderBox>
+      </HeaderContainer>
+      <empty></empty>
+    </HeaderSection>
+
   );
 }
 
